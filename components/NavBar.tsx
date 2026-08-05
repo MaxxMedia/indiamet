06fa9990622e643e67227dff40f023aa8bb847c0
BizTradeFairs <biztradefairs.com@gmail.com>
2026-08-05 12:48:04 +0530

    update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -231,7 +231,7 @@ export default function NavBar() {
 
 
             {/* ================= DESKTOP NAV BAR (UNCHANGED) ================= */}
-            <div className="hidden lg:block rounded-xl sm:rounded-2xl lg:rounded-3xl bg-gradient-to-r from-[#01163A] to-[#01163A] text-white shadow-xl">
+            <div className="hidden lg:block rounded-xl sm:rounded-2xl lg:rounded-3xl bg-gradient-to-r from-[#FFFFFF] to-[#01163A] text-white shadow-xl">
               <div className="flex items-center justify-between gap-1 sm:gap-2 md:gap-3 lg:gap-4 px-2 sm:px-3 md:px-4 lg:px-5 py-1.5 sm:py-2 md:py-2.5 lg:py-3">
 
                 {/* ================= LOGO SECTION ================= */}
