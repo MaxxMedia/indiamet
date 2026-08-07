977a93e111c0133058196fe6bccb99017c0fe8f9
BizTradeFairs <biztradefairs.com@gmail.com>
2026-08-07 21:30:34 +0530

    UPDATE

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -254,9 +254,9 @@ export default function NavBar() {
 
                   {/* Event Info - Hidden on small screens */}
                   <div className="md:flex flex-col font-parabolica min-w-0 ml-1 lg:ml-2">
-                    <span className="text-[12px] md:text-[14px] lg:text-[15px] xl:text-[18px] leading-tight whitespace-nowrap">
-                      22 - 24 APRIL 2027
-                    </span>
+                    <span className="text-[12px] md:text-[14px] lg:text-[15px] xl:text-[18px] leading-tight whitespace-nowrap font-bold">
+    22 - 24 APRIL 2027
+  </span>
                     <span className="text-[12px] md:text-[11px] lg:text-[15px] xl:text-[18px] leading-tight whitespace-nowrap truncate max-w-[150px] lg:max-w-[180px] xl:max-w-none">
                       AUTO CLUSTER EXHIBITION CENTER, PUNE, INDIA
                     </span>
