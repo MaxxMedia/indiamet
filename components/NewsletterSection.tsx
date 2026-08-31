22f2955c745341773f8550b8f7b51bdc0efb0a27
BizTradeFairs <biztradefairs.com@gmail.com>
2026-08-31 18:49:10 +0530

    update

--- components/NewsletterSection.tsx
+++ components/NewsletterSection.tsx
@@ -30,7 +30,7 @@ export default function NewsletterSection() {
 
           {/* IMAGE - Hidden on mobile, visible on lg+ */}
           <img
-            src="/images/newsletter.png"
+            src="/images/newsletter2.png"
             alt="Newsletter"
             className="pointer-events-none absolute bottom-0 right-0 z-10 hidden lg:block h-[200px] lg:h-[250px] xl:h-[300px] object-contain"
           />
